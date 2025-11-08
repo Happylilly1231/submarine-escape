74d71f323c7e9449cea5b4546dc9a01a1d3e6020
Happylilly <moon05lilly@gmail.com>
2025-11-09 02:44:20 +0900

    Feat: 애니메이션 기본 설정 & 1인칭 카메라 구현
    애니메이션 파일 6개 추가
    - Idle, 걷기, 달리기, 웅크려 걷기, 뛰면서 슬라이드, 구르고 뛰기
    애니메이션 잘 적용되는지 테스트
    1인칭 카메라 구현
    기타 폴더 구조 개선

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,11 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 2f;
-
     Rigidbody rb;
     CapsuleCollider col;
+
+    // 이동
     Vector2 moveInput;
+    [SerializeField] float moveSpeed = 2f;
+
+    // 시야 회전
+    Vector2 lookInput;
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float mouseSensitivity = 1f;
+    float xRotation = 0f; // 카메라 상하 회전 제한용
+
 
     void Awake()
     {
@@ -21,9 +29,28 @@ public class PlayerMove : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        // 좌우 회전
+        transform.Rotate(Vector3.up * mouseX);
+
+        // 상하 회전
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // 상하 회전 제한
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
     void FixedUpdate()
     {
-        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 moveDir = transform.right * moveInput.x + transform.forward * moveInput.y;
         rb.MovePosition(rb.position + moveDir * moveSpeed * Time.fixedDeltaTime);
     }
 }
